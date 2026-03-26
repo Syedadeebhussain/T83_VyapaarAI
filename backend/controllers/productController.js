0a090d256ac4164336dd95e6d92dbbc7d213fb15
Mohammad Ashhar <142567321+Mohammad-Ashhar@users.noreply.github.com>
2026-03-26 14:39:37 +0530

    Implement product retrieval and addition endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,1 +1,10 @@
-
+const Product=require("../models/Product");
+exports.getProducts=async(req,res)=>{
+    const products=await Product.find({shopId:req.params.shopId});
+    res.json(products);
+};
+exports.addProduct=async(req,res)=>{
+    const product=new Product(req.body);
+    await product.save();
+    res.json({message:"Product added"});
+};

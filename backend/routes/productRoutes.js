821c7b39cb13483278377a00ba50a5889a7a71b3
Mohammad Ashhar <142567321+Mohammad-Ashhar@users.noreply.github.com>
2026-03-26 14:38:42 +0530

    Set up product routes for getting and adding products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,1 +1,6 @@
-
+const express=require("express");
+const router=express.Router();
+const {getProducts,addProduct}=require("../controllers/productController");
+router.get("/:shopId",getProducts);
+router.post("/add",addProduct);
+module.exports=router;

1dcdce5c5586fb55807533fa8f610d3fbb11f945
Mohammad Ashhar <142567321+Mohammad-Ashhar@users.noreply.github.com>
2026-03-26 14:47:06 +0530

    Update shopRoutes.js

--- backend/routes/shopRoutes.js
+++ backend/routes/shopRoutes.js
@@ -1,1 +1,6 @@
-
+const express=require("express");
+const router=express.Router();
+const {registerShop,loginShop}=require("../controllers/shopController");
+router.post("/register",registerShop);
+router.post("/login",loginShop);
+module.exports=router;

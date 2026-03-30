35e114340c028b5db7e71bc528f92047cc059d89
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-03-30 15:58:27 +0530

    Update whatsappRoutes.js

--- backend/routes/whatsappRoutes.js
+++ backend/routes/whatsappRoutes.js
@@ -1,1 +1,11 @@
-routes/whatsappRoutes.js
+const express = require("express");
+const router = express.Router();
+const {
+  handleWebhook,
+  verifyWebhook,
+} = require("../controllers/whatsappController");
+
+router.get("/", verifyWebhook);
+router.post("/", handleWebhook);
+
+module.exports = router;
